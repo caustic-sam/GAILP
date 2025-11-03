56589cf27cc6bd9610fd8e413c4b38e49d9ce216
CAUSTIC <caustic_sam@protonmail.com>
2025-11-03 00:33:11 -0600

    fixing lint

--- app/policy-pulse/page.tsx
+++ app/policy-pulse/page.tsx
@@ -3,7 +3,6 @@
 
 import React from 'react';
 import Link from 'next/link';
-import { Header } from '@/components/Header';
 import { RightSidebar } from '@/components/RightSidebar';
 import { getFreshRSSClient, FreshRSSClient, FreshRSSItem } from '@/lib/freshrss';
 
