5d18a392ec70f7b0b9deff9772621a3986351ad8
CAUSTIC <caustic_sam@protonmail.com>
2025-11-03 01:43:11 -0600

    adding speed analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from 'next'
 import '../styles/globals.css'
@@ -27,6 +28,7 @@ export default function RootLayout({
           <RightSidebar />
           {children}
           <Analytics />
+          <SpeedInsights />
         </AuthProvider>
       </body>
     </html>
